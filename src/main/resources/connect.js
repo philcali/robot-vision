ef612aa4766a0f0e16c6b6430a37517609343762
Philip Cali <philip.cali@gmail.com>
2012-03-04 01:50:52 -0600

    Remote controlling

--- src/main/resources/connect.js
+++ src/main/resources/connect.js
@@ -15,16 +15,58 @@ window.onload = function() {
   var canvas = document.getElementById("desktop");
   var context = canvas.getContext("2d");
 
+  var mousepos = {};
+
   var host = "ws://" + window.location.host;
 
   var socket = new WebSocket(host);
 
+  canvas.onmousemove = function(evt) {
+    var x = evt.clientX - 9;
+    var y = evt.clientY - 8;
+    
+    mousepos = { x: x, y: y };
+    socket.send("mousemove|" + x + "|" + y);
+  };
+ 
+  canvas.onmousedown = function(evt) {
+    socket.send("mousedown|" + evt.button);
+  };
+
+  canvas.onmouseup = function(evt) {
+    socket.send("mouseup|" + evt.button);
+  };
+
+  document.onkeydown = function(evt) {
+    if (evt.preventDefault) {
+      evt.preventDefault();
+    }
+    socket.send("keydown|" + evt.keyCode);
+  };
+
+  document.onkeyup = function(evt) {
+    if (evt.preventDefault) {
+      evt.preventDefault();
+    }
+    socket.send("keyup|" + evt.keyCode);
+  };
+ 
+  var drawPointer = function(pos) {
+    context.beginPath();
+    context.arc(pos.x, pos.y, 3, 2 * Math.PI, false);
+    context.lineWidth = 2;
+    context.strokeStyle = "black";
+    context.stroke();
+  };
+
   socket.onmessage = function(e) {
     var image = new Image();
     image.onload = function() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
       context.drawImage(image, 0, 0);
+      drawPointer(mousepos);
     };
-    image.src = "/image/desktop.jpg";
+    image.src = "/image/desktop.jpg"
   };
 
   function fireNotification() {
--- src/main/scala/capture.scala
+++ src/main/scala/capture.scala
@@ -23,6 +23,10 @@ import java.awt.{
   Rectangle,
   Robot
 }
+import java.awt.event.{
+  InputEvent,
+  KeyEvent
+}
 import java.awt.image.RenderedImage
 import javax.imageio.{
   ImageIO,
@@ -32,15 +36,19 @@ import javax.imageio.{
 import javax.imageio.stream.MemoryCacheImageOutputStream
 import java.io.ByteArrayOutputStream
 
+object Remote {
+  private val robot = new Robot()
+
+  def apply[A](fun: Robot => A): Option[A] = allCatch opt(fun(robot))
+}
+
 object Screenshot {
   def screen = allCatch opt {
     val src = Toolkit.getDefaultToolkit.getScreenSize
     new Rectangle(src)
   } getOrElse (new Rectangle(800, 600))
 
-  def take = {
-    allCatch opt (new Robot().createScreenCapture(screen))
-  }
+  def take = Remote(_.createScreenCapture(screen))
 
   def data = take.map(toData).getOrElse(new Array[Byte](0))
 
@@ -62,6 +70,83 @@ object Screenshot {
   }
 }
 
+class EventParsing(val ident: String) {
+  def unapplySeq(text: String) = {
+    if (!text.startsWith(ident)) None else
+      Some(text.split("\\|").drop(1).toSeq)
+  }
+}
+
+trait MouseTranslator {
+  def translate(button: String) = button match {
+    case "0" => InputEvent.BUTTON1_MASK
+    case "2" => InputEvent.BUTTON3_MASK
+    case "1" => InputEvent.BUTTON2_MASK
+  }
+}
+
+trait KeyTranslator {
+  def translate(key: String) = key match {
+    case "65" => KeyEvent.VK_A
+    case "66" => KeyEvent.VK_B
+    case "67" => KeyEvent.VK_C
+    case "68" => KeyEvent.VK_D
+    case "69" => KeyEvent.VK_E
+    case "70" => KeyEvent.VK_F
+    case "71" => KeyEvent.VK_G
+    case "72" => KeyEvent.VK_H
+    case "73" => KeyEvent.VK_I
+    case "74" => KeyEvent.VK_J
+    case "75" => KeyEvent.VK_K
+    case "76" => KeyEvent.VK_L
+    case "77" => KeyEvent.VK_M
+    case "78" => KeyEvent.VK_N
+    case "79" => KeyEvent.VK_O
+    case "80" => KeyEvent.VK_P
+    case "81" => KeyEvent.VK_Q
+    case "82" => KeyEvent.VK_R
+    case "83" => KeyEvent.VK_S
+    case "84" => KeyEvent.VK_T
+    case "85" => KeyEvent.VK_U
+    case "86" => KeyEvent.VK_V
+    case "87" => KeyEvent.VK_W
+    case "88" => KeyEvent.VK_X
+    case "89" => KeyEvent.VK_Y
+    case "90" => KeyEvent.VK_Z
+    case "13" => KeyEvent.VK_ENTER
+    case "8" => KeyEvent.VK_BACK_SPACE
+    case "9" => KeyEvent.VK_TAB
+    case "32" => KeyEvent.VK_SPACE
+    case "16" => KeyEvent.VK_SHIFT
+    case "17" => KeyEvent.VK_CONTROL
+    case "18" => KeyEvent.VK_ALT
+    case "37" => KeyEvent.VK_LEFT
+    case "38" => KeyEvent.VK_UP
+    case "39" => KeyEvent.VK_RIGHT
+    case "40" => KeyEvent.VK_DOWN
+    case "48" => KeyEvent.VK_0
+    case "49" => KeyEvent.VK_1
+    case "50" => KeyEvent.VK_2
+    case "51" => KeyEvent.VK_3
+    case "52" => KeyEvent.VK_4
+    case "53" => KeyEvent.VK_5
+    case "54" => KeyEvent.VK_6
+    case "55" => KeyEvent.VK_7
+    case "56" => KeyEvent.VK_8
+    case "57" => KeyEvent.VK_9
+    case _ => KeyEvent.CHAR_UNDEFINED
+  }
+}
+
+object DesktopRender extends EventParsing("render")
+
+object MouseDown extends EventParsing("mousedown") with MouseTranslator
+object MouseUp extends EventParsing("mouseup") with MouseTranslator
+object MouseMove extends EventParsing("mousemove")
+
+object KeyUp extends EventParsing("keyup") with KeyTranslator
+object KeyDown extends EventParsing("keydown") with KeyTranslator
+
 object Lmxml extends Conversion {
   def createParser(step: Int) = new PlainLmxmlParser(step) with HtmlShortcuts
 }
@@ -82,8 +167,8 @@ object Main extends App {
     val screen = Screenshot.screen
 
     val trans = Transform(
-      "width" -> Value(screen.getWidth),
-      "height" -> Value(screen.getHeight)
+      "width" -> Value(screen.getWidth.toInt),
+      "height" -> Value(screen.getHeight.toInt)
     )
 
     val response = Lmxml.convert(source)(trans andThen XmlConvert).toString
@@ -102,8 +187,16 @@ object Main extends App {
     case _ => {
       case Message(s, Text(msg)) => msg match {
         case "render" => s.send("reload")
-        case "keypress" =>
-        case "mousemove" =>
+        case KeyUp(keycode) =>
+          Remote(_.keyRelease(KeyUp.translate(keycode)))
+        case KeyDown(keycode) =>
+          Remote(_.keyPress(KeyDown.translate(keycode)))
+        case MouseUp(button) =>
+          Remote(_.mouseRelease(MouseUp.translate(button)))
+        case MouseDown(button) =>
+          Remote(_.mousePress(MouseDown.translate(button)))
+        case MouseMove(xStr, yStr) =>
+          Remote(_.mouseMove(xStr.toInt, yStr.toInt))
       }
     }
   }).onPass(_.sendUpstream(_))).handler(unfiltered.netty.cycle.Planify {
