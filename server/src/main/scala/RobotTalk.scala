2312007c93b5f51ee1a9a1ce547e6efb8b545828
Philip Cali <philip.cali@gmail.com>
2012-04-22 14:15:47 -0500

    Clipboard backend

--- server/src/main/scala/RobotTalk.scala
+++ server/src/main/scala/RobotTalk.scala
@@ -13,16 +13,20 @@ class EventParsing(val ident: String) {
   }
 }
 
+// Mouse-related
 object MouseDown extends EventParsing("mousedown")
 object MouseUp extends EventParsing("mouseup")
 object MouseMove extends EventParsing("mousemove")
 
+// Keyboard-related
 object KeyUp extends EventParsing("keyup")
 object KeyDown extends EventParsing("keydown")
 
+// Other interaction
 object RobotAuth extends EventParsing("auth")
-
 object RobotRecord extends EventParsing("record")
+object ClipboardRetrieve extends EventParsing("clipretrieve")
+object ClipboardSet extends EventParsing("clipset")
 
 case class RobotTalk(secret: String) extends Plan with CloseOnException {
   @volatile private var controller: Option[java.lang.Integer] = None
@@ -73,18 +77,32 @@ case class RobotTalk(secret: String) extends Plan with CloseOnException {
   def pass: Plan.PassHandler = (_.sendUpstream(_)) 
 
   object RobotProtect {
-    def message(s: WebSocket, msg: String) = msg match {
+    def handleGeneral(s: WebSocket, msg: String) = msg match {
       case RobotRecord(value) => value match {
-          case "record" if !recorder.isRunning =>
-            recorder.start()
-            s.send("record|started")
-          case "stop" if recorder.isRunning =>
-            recorder.stop()
-            recorder = setupRecorder()
-            s.send("record|stopped")
-          case _ if recorder.isRunning => s.send("record|started")
-          case _ => s.send("record|stopped")
+        case "record" if !recorder.isRunning =>
+          recorder.start()
+          s.send("record|started")
+        case "stop" if recorder.isRunning =>
+          recorder.stop()
+          recorder = setupRecorder()
+          s.send("record|stopped")
+        case _ if recorder.isRunning => s.send("record|started")
+        case _ => s.send("record|stopped")
+      }
+      case ClipboardRetrieve(value) =>
+        s.send("clipboard|get|%s" format Clipboard.retrieve.getOrElse(""))
+      case msg if msg.startsWith(ClipboardSet.ident) =>
+        val value = ClipboardSet.unapplySeq(msg).map(_.mkString("|")).get
+        if (Clipboard(value).isDefined) {
+          s.send("clipboard|success")
+        } else {
+          s.send("clipboard|failure")
         }
+      case _ =>
+        handleInput(s, msg)
+    }
+
+    def handleInput(s: WebSocket, msg: String) = msg match {
       case KeyUp(KeyTranslate(keyCode)) =>
         Robot(_.keyRelease(keyCode))
       case KeyDown(KeyTranslate(keyCode)) =>
@@ -95,6 +113,10 @@ case class RobotTalk(secret: String) extends Plan with CloseOnException {
         Robot(_.mousePress(button))
       case MouseMove(xStr, yStr) =>
         Robot(_.mouseMove(xStr.toInt, yStr.toInt))
+      case _ =>
+        println("[DEBUG]: Unknown msg: %s" format msg)
     }
+
+    def message(s: WebSocket, msg: String) = handleGeneral(s, msg)
   }
 }
