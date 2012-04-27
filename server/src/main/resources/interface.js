d091407b2d78e9bb6b651a6f67036f44664d004a
Philip Cali <philip.cali@gmail.com>
2012-04-27 18:38:36 -0500

    UI for quality and scaling controls

--- server/src/main/resources/interface.js
+++ server/src/main/resources/interface.js
@@ -1,5 +1,15 @@
 $(function() {
-  var desktop = new Desktop('desktop', { pointer: true, interval: 200 });
+  var desktop = Desktop.get('desktop', { pointer: true, interval: 200 });
+
+  $('#controls-send').on('click', function() {
+    desktop.scaleY = $('.size').val();
+    desktop.scaleX = desktop.scaleY;
+    desktop.pointer = $('.pointer').is(':checked');
+    desktop.quality = $('.quality').val();
+
+    $('#controls-modal').modal('hide');
+    return false;
+  });
 
   // Desktop reload separate from animation cycle
   setTimeout(desktop.start, 1000);
